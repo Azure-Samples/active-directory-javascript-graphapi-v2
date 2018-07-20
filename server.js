32776e46410130366627a6da2a13ff507522f63f
andretms <andret@microsoft.com>
2018-07-20 14:45:49 -0700

    Update port number to 44316 in server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var morgan = require('morgan');
 var path = require('path');
 
 // Initialize variables.
-var port = 30662; // process.env.PORT || 30662;
+var port = 44316; // process.env.PORT || 44316;
 
 // Configure morgan module to log all requests.
 app.use(morgan('dev'));
