aa36a74789cea1b96e961947cd8c60ffc3c9949e
Navya Canumalla <nacanuma@microsoft.com>
2018-09-13 19:48:54 -0700

    Update redirect uri to match docs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var morgan = require('morgan');
 var path = require('path');
 
 // Initialize variables.
-var port = 44316; // process.env.PORT || 44316;
+var port = 30662; // process.env.PORT || 30662;
 
 // Configure morgan module to log all requests.
 app.use(morgan('dev'));
