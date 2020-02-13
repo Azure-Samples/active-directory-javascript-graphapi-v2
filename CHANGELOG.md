47a90bf4c82dcca1322f996b0006cd79db0e736e
Dogan Erisen <dogan.erisen@gmail.com>
2020-02-13 11:40:05 -0800

    adding readme and disclosures

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG
+
+## 2/13/2020
+
+* Source code refactored. Added new files: "auth.js", "graph.js" and "config.js"
+* Added new feature for reading user mails through MS Graph API.
+* UI redesigned with Bootstrap 4 framework.
+* Added AppCreationScripts for Powershell users.
--- JavaScriptSPA/config.js
+++ JavaScriptSPA/config.js
@@ -2,7 +2,7 @@
   // Config object to be passed to Msal on creation
   const msalConfig = {
     auth: {
-      clientId: 'Enter_the_Application_Id_Here',
+      clientId: "Enter_the_Application_Id_Here",
       authority: "Enter_the_Cloud_Instance_Here/Enter_the_Tenant_Info_Here",
       redirectUri: "Enter_the_Redirect_Uri_Here",
     },
