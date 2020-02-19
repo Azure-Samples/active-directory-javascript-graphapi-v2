5cea5b18debaf0af09586a7bdfa65ece3bbea517
Dogan Erisen <dogan.erisen@gmail.com>
2020-02-19 11:52:17 -0800

    apps.json corrections

--- JavaScriptSPA/graphConfig.js
+++ JavaScriptSPA/graphConfig.js
@@ -1,7 +1,7 @@
   // Add here the endpoints for MS Graph API services you would like to use.
   const graphConfig = {
-    graphMeEndpoint: "Enter_the_Graph_Endpoint_Here/v1.0/me",
-    graphMailEndpoint: "Enter_the_Graph_Endpoint_Here/v1.0/me/messages"
+    graphMeEndpoint: "Enter_the_Graph_Endpoint_Herev1.0/me",
+    graphMailEndpoint: "Enter_the_Graph_Endpoint_Herev1.0/me/messages"
   };
 
   // Add here scopes for access token to be used at MS Graph API endpoints.
